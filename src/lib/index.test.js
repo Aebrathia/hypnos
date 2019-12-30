7b7c5fcdc431b8aec64a5605b301b87244877d03
Andrea Maugars <andrea.maugars@gmail.com>
2019-12-30 09:39:22 +0100

    Add first test

---
+++ src/lib/index.test.js
@@ -0,0 +1,7 @@
+import { capitalize } from './index.js';
+
+describe('capitalize', () => {
+  it('Capitalizes the first letter and ignores the rest', () => {
+    expect(capitalize('zeus')).toBe('Zeus');
+  });
+});
